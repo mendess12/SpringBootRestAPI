8bed6e651934f5ef296ee776962fecfa632ab0a1
yusufmendes <yusufmendes123@gmail.com>
2025-04-18 14:34:14 +0300

    Use of request params with rest api

--- src/main/java/com/yusufmendes/controller/RestEmployeeController.java
+++ src/main/java/com/yusufmendes/controller/RestEmployeeController.java
@@ -3,27 +3,32 @@ package com.yusufmendes.controller;
 import com.yusufmendes.model.Employee;
 import com.yusufmendes.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/rest/api")
+@RequestMapping("/rest/api/employee")
 public class RestEmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping(path = "/employee-list")
+    @GetMapping(path = "/list")
     public List<Employee> getAllEmployeeList() {
         return employeeService.getAllEmployeeList();
     }
 
-    @GetMapping(path = "/employee-list/{id}")
+    @GetMapping(path = "/list/{id}")
     public Employee getEmployeeById(@PathVariable(name = "id", required = true) String id) {
         return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping("/with-params")
+    public List<Employee> getEmployeeWithParams(
+            @RequestParam(name = "firstName", required = false) String firstName,
+            @RequestParam(name = "lastName", required = false) String lastName
+    ) {
+        return employeeService.getEmployeeWithParams(firstName, lastName);
     }
 }
--- src/main/java/com/yusufmendes/repository/EmployeeRepository.java
+++ src/main/java/com/yusufmendes/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.yusufmendes.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,5 +28,34 @@ public class EmployeeRepository {
             }
         }
         return findEmployee;
+    }
+
+    public List<Employee> getEmployeeWithParams(String firstName, String lastName) {
+        List<Employee> employeeWithParams = new ArrayList<>();
+
+        if (firstName == null && lastName == null) {
+            return employeeList;
+        }
+
+        for (Employee employee : employeeList) {
+            if (firstName != null && lastName != null) {
+                if (employee.getFirstName().equalsIgnoreCase(firstName) && employee.getLastName().equalsIgnoreCase(lastName)) {
+                    employeeWithParams.add(employee);
+                }
+            }
+            if (firstName != null && lastName == null) {
+                if (employee.getFirstName().equalsIgnoreCase(firstName)) {
+                    employeeWithParams.add(employee);
+                }
+
+            }
+
+            if (lastName != null && firstName == null) {
+                if (employee.getFirstName().equalsIgnoreCase(lastName )) {
+                    employeeWithParams.add(employee);
+                }
+            }
+        }
+        return employeeWithParams;
     }
 }
